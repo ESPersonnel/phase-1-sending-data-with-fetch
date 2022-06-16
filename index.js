3d9370e969ee093dfbb6d80912b2e89bb4da239d
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-16 12:32:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify( {
+      name: name,
+      email: email
+    } )
+  } )
+    .then( response => response.json() )
+    .then( data => {
+      let id = data.id // get the id from the response
+      document.body.innerHTML += `<p>${id}</p>` // append the id to the DOM
+    } )
+    .catch( error => { // handle the error
+      document.body.innerHTML += `<p>${error.message}</p>` // append the error message to the DOM
+    } )
+}
